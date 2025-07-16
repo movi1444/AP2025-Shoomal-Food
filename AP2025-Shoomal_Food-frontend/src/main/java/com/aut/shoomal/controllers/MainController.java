f470a4e2dc7343278bbf23d9facdde785230f5cd
Mr.Erfan <erfan.rajabi1385@gmail.com>
2025-07-16 17:59:08 +0330

    basic buyer profile

--- AP2025-Shoomal_Food-frontend/src/main/java/com/aut/shoomal/controllers/MainController.java
+++ AP2025-Shoomal_Food-frontend/src/main/java/com/aut/shoomal/controllers/MainController.java
@@ -2,11 +2,9 @@ package com.aut.shoomal.controllers;
 
 import javafx.fxml.FXML;
 import javafx.scene.Node;
-import javafx.scene.control.Label;
-import javafx.scene.control.ScrollPane;
+import javafx.scene.control.*;
 import javafx.scene.layout.VBox;
 import javafx.scene.layout.StackPane;
-import javafx.scene.control.Button;
 import javafx.event.ActionEvent;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -26,7 +24,6 @@ import javafx.scene.control.Alert.AlertType;
 import java.io.IOException;
 import javafx.scene.Cursor;
 import javafx.scene.shape.Circle;
-import javafx.scene.control.Tooltip;
 import com.aut.shoomal.dto.response.ApiResponse;
 
 public class MainController extends AbstractBaseController {
@@ -41,11 +38,21 @@ public class MainController extends AbstractBaseController {
     @FXML private VBox defaultView;
     @FXML private ImageView profilePictureImageView;
 
+    //Seller
+    @FXML private VBox sellerLinksContainer;
+    @FXML private Hyperlink createRestaurantLink;
+    @FXML private Hyperlink showRestaurantLink;
+    @FXML private Hyperlink manageMenusLink;
+    @FXML private Hyperlink manageFoodsLink;
+    @FXML private Hyperlink manageOrdersLink;
+
     private UserResponse currentUser;
+    private String token;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         super.initialize(url, resourceBundle);
+        token = PreferencesManager.getJwtToken();
         hideAllDashboards();
         if (defaultView != null) {
             defaultView.setVisible(true);
@@ -148,6 +155,11 @@ public class MainController extends AbstractBaseController {
                 break;
             case "seller":
                 targetPane = sellerDashboardScrollPane;
+                if (sellerLinksContainer != null)
+                {
+                    sellerLinksContainer.setVisible(true);
+                    sellerLinksContainer.setManaged(true);
+                }
                 break;
             case "courier":
                 targetPane = courierDashboardScrollPane;
